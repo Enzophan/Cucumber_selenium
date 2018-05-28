8f64afeb469e08d3642eee92a428cbe9b0f14072
Nhan Phan Le Hien <nhan.phan@QGSVN.local>
2018-05-28 18:32:49 +0700

    Update

--- src/test/java/com/example/webExampleSteps.java
+++ src/test/java/com/example/webExampleSteps.java
@@ -12,6 +12,8 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class webExampleSteps {
 
     WebDriver driver;
@@ -22,23 +24,53 @@ public class webExampleSteps {
         System.setProperty("webdriver.gecko.driver", "E://Testing//geckodriver.exe");
         driver= new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.get("https://wb.lab.qup.vn/booking.html?fleet=ecar");
+//        driver.get("https://wb.lab.qup.vn/booking.html?fleet=ecar");
+        driver.get("https://wb.beta.qup.vn/booking.html?fleet=qa");
         WebElement myDynamicElement = (new WebDriverWait(driver, 20))
                 .until(ExpectedConditions.presenceOfElementLocated(By.id("contentStep1")));
         System.out.println("This Step open the firefox.");
     }
 
-    @Given("^Book Reservation$")
+    @Given("^Login Web Booking page$")
+    public void login_web_booking() throws Throwable
+    {
+        driver.findElement(By.className("btLogin")).click();
+        System.out.println("Accept to Login page.");
+    }
+
+    @When("^Enter Location to Booking one trip$")
     public void waiting_for_loading_successful() throws  Throwable
     {
-    //    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement pickupLocation = driver.findElement(By.name("pickup"));
+        pickupLocation.sendKeys("San Bay Quoc Te Da Nang");
+        Thread.sleep(3000);
+        pickupLocation.sendKeys(Keys.ARROW_DOWN);
+        pickupLocation.sendKeys(Keys.RETURN);
+
+
+        WebElement destinationLocation = driver.findElement(By.name("destination"));
+        destinationLocation.sendKeys("Hoi An");
+        Thread.sleep(3000);
+        destinationLocation.sendKeys(Keys.ARROW_DOWN);
+        destinationLocation.sendKeys(Keys.RETURN);
+
+        Thread.sleep(3000);
+        driver.findElement(By.name("btn-book-a-ride")).click();
+        System.out.println("Enter location for booking successful");
+    }
+
+    @When ("^Select Car Type for Trip$")
+    public void select_car_type_for_trip() throws Throwable
+    {
+        System.out.println("This Step Select Car Type.");
 
     }
 
-    @When("^Enter the Username and Password$")
+    @When("^Enter the Username and Password on Web Booking$")
     public void enter_the_Username_and_Password() throws Throwable
     {
-        driver.findElement(By.className("btLogin")).click();
+//        driver.findElement(By.className("btLogin")).click();
         WebElement phoneNumber = driver.findElement(By.cssSelector("input.form-control:nth-child(2)"));
         phoneNumber.sendKeys(Keys.CONTROL + "a");
         phoneNumber.sendKeys(Keys.DELETE);
@@ -50,11 +82,30 @@ public class webExampleSteps {
         WebElement signIn = driver.findElement(By.cssSelector(".submitLogin"));
         signIn.click();
 
-
-
 //        driver.findElement(By.name("uid")).sendKeys("username12");
 //        driver.findElement(By.name("password")).sendKeys("password12");
         System.out.println("This Step enter User and Password.");
+    }
+
+    @When("^Enter the list username and password$")
+    public void enter_the_list_username_and_password() throws Throwable
+    {
+        System.out.println("This step verify list username and password");
+    }
+
+    @Then("^Logout page$")
+    public void logout_page() throws Throwable
+    {
+        driver.findElement(By.cssSelector(".glyphicon-log-out")).click();
+        System.out.println("This Step Logout button.");
+    }
+
+    @Then("^Booked successful$")
+    public void select_car_type_and_type_to_booking() throws Throwable
+    {
+        System.out.println("Booked.");
+        Thread.sleep(3000);
+        driver.quit();
     }
 
     @Then("^Reset the credential$")
--- target/test-classes/MyTest.feature
+++ target/test-classes/MyTest.feature
@@ -1,13 +1,25 @@
 Feature: Reset functionality on login page of Application
 
 
-  Scenario: Verification of Reset button
+#  Scenario: 1. Verification of Login/Logout button on Web Booking
+#    Given Open the Firefox and launch the application
+#    And Login Web Booking page
+#    When Enter the Username and Password on Web Booking
+#    Then Logout page
 
+#  Scenario: 1. Verification of Login function on Web Booking
+#    Given Open the Firefox and launch the application
+#    And Login Web Booking page
+#    When Enter the list username and password
+#    Examples:
+#    |username      |password     |
+#    |+84901223344  |demo@12345   |
+#    Then Logout page
+
+
+  Scenario: 2. Verification of booking one trip on Web Booking
 
     Given Open the Firefox and launch the application
-
-
-    When Enter the Username and Password
-
-
-    Then Reset the credential
+    When Enter Location to Booking one trip
+    And Select Car Type for Trip
+    Then Booked successful
